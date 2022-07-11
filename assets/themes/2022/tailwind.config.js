451bbb2fbe58cd83585ee1dda17aa987f708b56d
misostack <misostack.com@gmail.com>
2022-07-11 18:13:25 +0700

    add styles for theme

---
+++ assets/themes/2022/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: ["./*.php", "./**/*.php"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#90cf43",
+      },
+      fontFamily: {
+        body: ["arial"],
+        heading: ["'Merriweather', serif"],
+      },
+      textColor: {
+        primary: "#90cf43",
+      },
+      backgroundColor: {
+        primary: "#8cc43d",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
